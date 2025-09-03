b43e8da01563f09f2ca45d0b7fb59f98be88301e
Mobilizes <fernando142311@gmail.com>
2025-09-03 17:42:33 +0700

    refactor: switch init structure

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,18 @@
+vim.keymap.set('n', '<leader>E', ':NvimTreeToggle<CR>')
+vim.keymap.set('n', '<C-h>', '<C-w><Left>')
+vim.keymap.set('n', '<C-l>', '<C-w><Right>')
+vim.keymap.set('n', '<C-j>', '<C-w><Down>')
+vim.keymap.set('n', '<C-k>', '<C-w><Up>')
+
+vim.keymap.set('n', '<leader>f', ':FzfLua files<CR>')
+
+vim.keymap.set('n', '<C-s>', ':w<CR>')
+
+vim.keymap.set('v', '<leader>cf', vim.lsp.buf.format, bufopts)
+vim.keymap.set('v', '<leader>cF', vim.lsp.buf.format)
+
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+
+vim.keymap.set('n', '<leader>b', '<cmd>Bufferin<cr>', { desc = 'Toggle Bufferin' })
---
+++ lua/plugins/bufferin.lua
@@ -0,0 +1,10 @@
+return {
+  'wasabeef/bufferin.nvim',
+  cmd = { "Bufferin" },
+  config = function()
+    require('bufferin').setup()
+  end,
+  dependencies = {
+    'nvim-tree/nvim-web-devicons', -- For file icons
+  }
+}
