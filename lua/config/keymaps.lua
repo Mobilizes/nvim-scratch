69a2ac2c98dace5e88ded2385fb929cc303a32b8
Mobilizes <fernando142311@gmail.com>
2025-09-05 15:00:49 +0700

    feat: add git diff

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,11 +5,12 @@ vim.keymap.set('n', '<C-j>', '<C-w><Down>')
 vim.keymap.set('n', '<C-k>', '<C-w><Up>')
 
 vim.keymap.set('n', '<C-s>', ':w<CR>')
+vim.keymap.set('n', '<C-o>', 'ddO')
 
-vim.keymap.set({'n', 'v'}, '<leader>c', '', {desc = 'Code'})
-vim.keymap.set('v', '<leader>cf', vim.lsp.buf.format, bufopts)
-vim.keymap.set('v', '<leader>cF', vim.lsp.buf.format)
-vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+vim.keymap.set({ 'n', 'v' }, '<leader>c', '', { desc = 'Code' })
+vim.keymap.set('v', '<leader>cf', vim.lsp.buf.format, bufopts, { desc = 'Format selection' })
+vim.keymap.set('n', '<leader>cF', vim.lsp.buf.format, { desc = 'Format all' })
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code actions' })
 
 vim.keymap.set('n', '<leader>l', ':Lazy<cr>', { desc = 'Lazy Manager' })
 vim.keymap.set('n', '<leader>m', ':Mason<cr>', { desc = 'Mason Manager' })
